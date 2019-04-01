076af7fbce8f146c55d56ea22f06a3d3854ad31c
khizarSulehria <khizar.sulehri.ks@gmail.com>
2019-04-01 23:05:01 +0500

    create relationship between Reviews and Product

--- eapi/app/Model/Product.php
+++ eapi/app/Model/Product.php
@@ -3,8 +3,16 @@
 namespace App\model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\model\Reviews;
 
 class Product extends Model
 {
     //
+    protected $fillable = [
+		'name','detail','stock','price','discount'
+    ];
+    public function reviews()
+    {
+    	return $this->hasMany(Review::class); 
+    }
 }
--- eapi/app/Model/Review.php
+++ eapi/app/Model/Review.php
@@ -3,8 +3,16 @@
 namespace App\model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\model\Product;
 
 class Review extends Model
 {
     //
+    protected $fillable = [
+		'star','customer','review'
+    ];
+    public function product()
+    {
+    	return $this->belongsTo(Product::class); 
+    }
 }
