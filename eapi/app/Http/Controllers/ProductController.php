eb27906b5d4c78b7cb589e38278a70d99f020aca
khizarSulehria <khizar.sulehri.ks@gmail.com>
2019-04-09 02:04:04 +0500

    Product Collection Resource

--- eapi/app/Http/Controllers/ProductController.php
+++ eapi/app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\model\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\Product\ProductResource;
+use App\Http\Resources\Product\ProductCollection;
 
 class ProductController extends Controller
 {
@@ -16,7 +17,8 @@ class ProductController extends Controller
     public function index()
     {
         //
-        return Product::all();
+        //return Product::all();
+        return ProductCollection::collection(Product::all());
     }
 
     /**
